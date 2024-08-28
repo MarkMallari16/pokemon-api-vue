750840823d44cf7a639fbd076279d1cb47a7df70
MarkMallari16 <mallariftw01@gmail.com>
2024-08-28 11:34:59 +0800

    initial commit

---
+++ second-vue-project/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { onMounted, ref, watch } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+
+const dragonBalls = ref({});
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://dragonball-api.com/api/characters");
+    const data = await response.json();
+
+    dragonBalls.value = [data];
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+  }
+});
+watch(dragonBalls, async (newDragonBalls) => {
+  console.log(newDragonBalls);
+});
+
+console.log(dragonBalls.value);
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
