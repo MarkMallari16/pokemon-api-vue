323bf583ba9f6cb754a95c7e9185fc79f413857d
MarkMallari16 <mallariftw01@gmail.com>
2024-08-29 08:03:39 +0800

    change message for sample

--- second-vue-project/src/App.vue
+++ second-vue-project/src/App.vue
@@ -27,7 +27,7 @@ console.log(dragonBalls.value);
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Dragon Ball" />
     </div>
   </header>
 
