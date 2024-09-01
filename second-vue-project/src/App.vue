bb505ef54f8375856c931dcce708815906622d0e
MarkMallari16 <mallariftw01@gmail.com>
2024-09-01 14:19:18 +0800

    practicing crud

--- second-vue-project/src/App.vue
+++ second-vue-project/src/App.vue
@@ -1,40 +1,39 @@
 <script setup>
 import { onMounted, ref, watch } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
 
 let todosData = ref([]);
 
 onMounted(async () => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
     const data = await response.json();
-
 
     todosData.value = data;
   } catch (error) {
     console.error("Error fetching data:", error); // Handle any errors
   }
 });
+const deleteTodo = (id) => {
+  todosData.value = todosData.value.filter((todo) => todo.id !== id);
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Dragon Ball" />
-    </div>
-  </header>
-
-  <main>
+  <main class="p-5">
+    <h1 class="text-2xl font-bold mt-5">PokemonAPI</h1>
     <div v-if="todosData.length">
-      <ul>
-        <li v-for="todo in todosData" :key="todo.id">
+      <ul class="flex justify-start flex-wrap">
+        <li
+          class="p-5 bg-gray-300 m-2 rounded-lg"
+          v-for="todo in todosData"
+          :key="todo.id"
+        >
           {{ todo.title }}
+          <button class="bg-red-500 p-2 rounded-lg" @click="deleteTodo(todo.id)">
+            Delete
+          </button>
         </li>
       </ul>
     </div>
-    <p v-else>Loading...</p>
   </main>
 </template>
